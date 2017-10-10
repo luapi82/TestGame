9857e5d82036dc9bdb621b674bbd0674ee032805
luapi82 <paulviau@gmail.com>
2017-10-10 16:51:22 -0400

    Game - Make enough money within 5 days
    Must install python to run game.
    Make sure to check add to path during installation.
    Work in Progress...

---
+++ game.py
@@ -0,0 +1,157 @@
+#The goal is to double the money you borrowed and make as much as possible within 5 days. Every action will take away hours left until you're forced to sleep.
+import sys
+import win32com.client as wincl
+speak = wincl.Dispatch("SAPI.SpVoice")
+
+playerHP = 100
+moneyInBank = 0
+moneyInPocket = 0
+borrowedAmount = 0
+moneyOwed = 0
+hoursLeft = 18
+daysLeft = 5
+workedToday = False
+
+def pressEnterToContinue():
+	nL()
+	str(input('Press Enter to continue...'))
+
+def nL():
+	print()
+
+def showStats():
+	nL()
+	print("-----------------------------------------------------------------------------------------------------\n[ Your HP:",playerHP,"] [ Money in pocket:",moneyInPocket,"] [ Money in Bank:",moneyInBank,"] [ Money Owed:",moneyOwed,"] [ Days Left:",daysLeft,"]\n-----------------------------------------------------------------------------------------------------")
+
+def youWon():
+	nL()
+	print('You won!')
+	nL()
+	str(input('Press enter to exit game...'))
+	return
+
+def gameOver():
+	nL()
+	print('You lost')
+	pressEnterToContinue()
+	return
+	
+def fight():
+#Can only fight as long as you have HP
+	global moneyInPocket
+	global moneyOwed
+	from random import randint
+	enemyHP = randint(20,80)
+	moneyOnTheTable = int((enemyHP/10))
+	showStats()
+	nL()
+	print('This fight will be worth [$',moneyOnTheTable,'] You currently have [$',moneyInPocket,'].')
+	if moneyOnTheTable <= moneyInPocket:
+		userChoice=str(input('Are you sure you want to bet that amount? [Y,N]: '))
+		if (userChoice == 'y' or userChoice == 'Y'):
+			moneyInPocket -= moneyOnTheTable
+	else:
+		print("Sorry bud, you don't have enough money for this fight. Go get a job!")
+		pressEnterToContinue()
+	mainGameChoice()
+	
+def gamble():
+#Can gamble as much as you want
+	nL()
+	print('gamble')
+	
+def workPartTime():
+#Can only work once a day
+	global moneyInPocket
+	global workedToday
+	from random import randint
+	hours = randint(2,7)
+	wage = 6
+	if workedToday == False:
+		nL()
+		gained = (hours * wage)
+		moneyInPocket += gained
+		print('You take a [',hours,'] hour shift at McDonalds and gained $[',gained,']')
+		workedToday = True
+		pressEnterToContinue()
+		mainGameChoice()
+	else:
+		nL()
+		print('You don\'t have any shifts left for today. Come back tomorrow!')
+		mainGameChoice()
+	
+def sleep():
+#Sleep replenishes all HP
+	global playerHP
+	global workedToday
+	global daysLeft
+	global moneyInBank
+	global moneyInPocket
+	global moneyOwed
+	if (daysLeft > 0):
+		workedToday	= False
+		nL()
+		print('You go to sleep... You wake up the next day feeling replenished.')
+		playerHP = 100
+		daysLeft -= 1
+		pressEnterToContinue()
+		mainGameChoice()
+	elif (daysLeft == 0):
+		if (moneyInBank + moneyInPocket >= moneyOwed):
+			youWon()
+		else: 
+			nL()
+			userChoice=str(input('You don\'t have enough money to win the game. Are you sure you want to finish the game now? [Y,N]: '))
+			if (userChoice == 'y' or userChoice == 'Y' or userChoice == 'yes' or userChoice == 'Yes' or userChoice == 'YES'):gameOver()
+			else:mainGameChoice()
+	
+def banking():
+#Can deposit for a 20% fee
+#Can Withdraw
+	global moneyInPocket
+	global moneyInBank
+	costToDeposit = 1.20
+	nL()
+	print('banking')
+	pressEnterToContinue()
+	mainGameChoice()
+	
+def exitGame():
+	nL()
+	userChoice=str(input('Are you sure you want to exit? [Y,N]: '))
+	if (userChoice == 'y' or userChoice == 'Y' or userChoice == 'yes' or userChoice == 'Yes' or userChoice == 'YES'):sys.exit()
+	else:mainGameChoice()
+
+def mainGameChoice():
+	showStats()
+	nL()
+	print("What would you like to do?")
+	while True:
+		userChoice=int(input('[1]Fight, [2]Gamble, [3]Work part time, [4]Sleep, [5]Banking, [6]Exit Game [1-6]:'))
+		if (userChoice == 1 or userChoice == 2 or userChoice == 3 or userChoice == 4 or userChoice == 5 or userChoice == 6):
+			break
+	if (userChoice == 1):
+		fight()
+	elif (userChoice == 2):gamble()
+	elif (userChoice == 3):workPartTime()
+	elif (userChoice == 4):sleep()
+	elif (userChoice == 5):banking()
+	elif (userChoice == 6):exitGame()
+
+def startGame():
+	global moneyInPocket
+	global moneyOwed
+	nL()
+	message = "You left home. If you can call it that... You have no money. Desperate you borrowed money from someone who was willing to lend it but the catch is to return twice the amount of money within 5 days."
+	print (message)
+	#speak.Speak(message)
+	nL()
+	while True:
+		userChoice=int(input('How much money would you like to borrow? $[500-1000]: '))
+		if (userChoice <= 1000 and userChoice >= 500):
+			break
+	moneyOwed = userChoice * 2
+	moneyInPocket = userChoice
+	mainGameChoice()
+
+startGame()
